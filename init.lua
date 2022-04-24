0a072a8c5f165866bee623ea81e3835b8f66e427
Dirkfried <>
2022-04-24 13:18:08 +0200

    tidy up

--- init.lua
+++ init.lua
@@ -3,11 +3,9 @@ local path = minetest.get_modpath("custom") .. "/src"
 
 -- stuff
 dofile(path .. "/armor.lua")
---dofile(path .. "/nodes.lua")
---dofile(path .. "/mapgen.lua")
 --dofile(path .. "/commands.lua")
 --dofile(path .. "/crafting.lua")
+dofile(path .. "/functions.lua")
+dofile(path .. "/mobs.lua")
 --dofile(path .. "/register.lua")
 dofile(path .. "/tools.lua")
-dofile(path .. "/mobs.lua")
-dofile(path .. "/functions.lua")
