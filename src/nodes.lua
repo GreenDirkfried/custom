277e8f83a866f29fbad5dd23b0624b58406029ac
Dirkfried <>
2024-11-01 18:38:47 +0100

    add air ladder from the loud walking mod

---
+++ src/nodes.lua
@@ -0,0 +1,17 @@
+--additional nodes
+-- taken with texture from the mod "Loud Walking Mapgen" from duane
+minetest.register_node("custom:air_ladder", {
+	description = "Air Ladder",
+	drawtype = "glasslike",
+	tiles = {"loud_walking_air_ladder.png"},
+	paramtype = "light",
+	sunlight_propagates = true,
+	walkable = false,
+	use_texture_alpha = true,
+	climbable = true,
+	is_ground_content = false,
+	selection_box = {
+		type = "fixed",
+		fixed = {0, 0, 0, 0, 0, 0},
+	},
+})
