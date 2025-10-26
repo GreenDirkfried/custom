414e81710f2d139dd9e445517d8444acccf85205
Dirkfried <>
2025-10-26 10:38:02 +0100

    added unbreakable nodes as in maptools

--- src/nodes.lua
+++ src/nodes.lua
@@ -1,5 +1,7 @@
 --additional nodes
 
+custom = {}
+
 local S = minetest.get_translator("custom")
 
 -- taken with texture from the mod "Loud Walking Mapgen" from duane
@@ -26,4 +28,53 @@ minetest.register_node("custom:stone_with_nether", {
 	groups = {cracky = 2, level = 2},
 	drop = "custom:nether_lump",
 	sounds = default.node_sound_stone_defaults(),
+})
+
+--chat message for no drop of unbreakable nodes
+custom.unbreakable_drop_msg = function(itemstack, player)
+	local name = player:get_player_name()
+	minetest.chat_send_player(name, S("[custom unbreakable] nodes do not drop!"))
+end
+
+--function for faster unbreakable node registration
+function custom.register_unbreakable_node(name, def)
+	def.drop = ""
+	if def.groups then
+		if not def.groups.dig_immediate then
+			def.groups.unbreakable = 1
+		end
+		def.groups.not_in_creative_inventory = 1
+	else
+		def.groups = {unbreakable = 1, not_in_creative_inventory = 1}
+	end
+	def.on_drop = custom.unbreakable_drop_msg
+	-- Prevent nodes from being exploded by TNT.
+	def.on_blast = function() end
+	if minetest.get_modpath("mesecons_mvps") then
+		mesecon.register_mvps_stopper(name)
+	end
+	def.is_ground_content = false
+	minetest.register_node(name, def)
+end
+
+--default nodes in unbreakable
+
+custom.register_unbreakable_node("custom:unbreakable_obsidian", {
+	description = S("Unbreakable Obsidian"),
+	tiles = {"default_obsidian.png"},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+custom.register_unbreakable_node("custom:unbreakable_obsidian_block", {
+	description = S("Unbreakable Obsidian Block"),
+	tiles = {"default_obsidian_block.png"},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+custom.register_unbreakable_node("custom:unbreakable_ice", {
+	description = S("Unbreakable Ice"),
+	tiles = {"default_ice.png"},
+	paramtype = "light",
+	groups = {cools_lava = 1, slippery = 3},
+	sounds = default.node_sound_ice_defaults(),
 })
